ee276477141910e742a5686c7c3376f2ebb72166
amirping <amirs-m-s@live.com>
2019-04-05 07:48:41 +0100

    dashboard working on

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
-import { Box, Text, Heading } from "grommet";
-import { IconButton, Icon } from "@material-ui/core";
+import { Box, Text, Heading, Paragraph } from "grommet";
+import { IconButton, Icon, CircularProgress } from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
 import "./Dashboard.css";
+import img_Pick from "./pick.png";
+import img_Boost from "./animat-rocket-color.gif";
 import ListItem from "../list/ListItem";
 import DashboardConfig from "../dashboardConfig/DashboardConfig";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as dashboardsActions from "../../actions/dashboards";
 export interface DashboardProps {}
 
 export interface DashboardState {
   data: any;
 }
 
-class Dashboard extends Component<DashboardProps, DashboardState> {
-  constructor(props: DashboardProps) {
+class Dashboard extends Component<any, DashboardState> {
+  constructor(props: any) {
     super(props);
     this.state = {
       data: {
@@ -57,7 +62,7 @@ class Dashboard extends Component<DashboardProps, DashboardState> {
           border="bottom"
           gap="10px">
           <Text size="xxlarge" truncate={true} className="dash-name">
-            {this.state.data.name}
+            {this.props.dashboard_data.title}
           </Text>
           <DashboardConfig />
         </Box>
@@ -77,11 +82,22 @@ class Dashboard extends Component<DashboardProps, DashboardState> {
       </Box>
     );
   };
-
+  DataLoader = () => {
+    return this.props.dashboard_data &&
+      this.props.dashboard_data.length !== 0 ? (
+      <this.DashRender />
+    ) : (
+      <Box fill flex direction="column" justify="around">
+        <Box direction="row" justify="around">
+          <CircularProgress color="secondary" style={{ margin: "auto" }} />
+        </Box>
+      </Box>
+    );
+  };
   render() {
     return (
       <Box direction="row" fill background="dark-2">
-        {this.state.data && this.state.data != null ? (
+        {/* {this.state.data && this.state.data != null ? (
           <this.DashRender />
         ) : (
           <Box>
@@ -89,10 +105,52 @@ class Dashboard extends Component<DashboardProps, DashboardState> {
               Select a dashbord from the list at the left or create a new one
             </Text>
           </Box>
+        )} */}
+        {this.props.selected_dashboard &&
+        this.props.selected_dashboard.length !== 0 ? (
+          <this.DataLoader />
+        ) : (
+          <Box fill flex direction="column" justify="around">
+            <Box direction="row" justify="around">
+              <img
+                src={img_Boost}
+                alt="where are you"
+                height="400px"
+                style={{ opacity: 0.5 }}
+              />
+            </Box>
+            <Box fill="horizontal" direction="row" justify="around">
+              <Paragraph margin="small" size="large">
+                Hi There, happy to see you here 😀. you can start by creating
+                adashbaord if you haven't . else select on from your dashboards
+                to start the show
+              </Paragraph>
+            </Box>
+            {/* <img src="https://i.gifer.com/yH.gif" alt="where are you" /> */}
+          </Box>
         )}
       </Box>
     );
   }
 }
+const mapStateToProps = (state: any) => {
+  return {
+    success: state.dashboards.success,
+    error: state.dashboards.error,
+    selected_dashboard: state.dashboards.selectedDashboardID,
+    dashboard_data: state.dashboards.selectedDashboardData
+  };
+};
 
-export default Dashboard;
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    // openCreator: () => dispatch(openDashboardCreator()),
+    // closeCreator: () => dispatch(closeDashboardCreator()), --> will changed by dashconfig
+    actions: bindActionCreators(dashboardsActions, dispatch)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard);
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,24 +3,6 @@ import view from "./view";
 import net from "./net";
 import auth from "./auth";
 import dashboards from "./dashboards";
-// const initialState = {
-//   dashboardsList: {
-//     dashboardsIDs: [],
-//     dashboards: {}
-//   },
-//   sideMenu: true,
-//   connected: false,
-//   user: null,
-//   dashboardTocreate: {
-//     id: "",
-//     title: "",
-//     lists: []
-//   },
-//   selectedDashboardID: null,
-//   selectedDashboardData: null,
-//   success: null,
-//   error: null
-// };
 
 const rootReducer = combineReducers({
   // short hand property names
