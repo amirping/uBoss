77ac7601dafaa5bda4f07d774ca1b7fff79a9701
amirping <amirs-m-s@live.com>
2019-04-05 07:47:04 +0100

    showing and loading

---
+++ src/actions/dashboard.ts
@@ -0,0 +1,5 @@
+// basically it's off we may have to work on dashboards since it have all apossible action on the dashboard
+import * as Types from "./actionTypes";
+import dashboardApi from "../api/dashboardApi";
+import { netError } from "./net";
+export function loadDashboards(dashboard: any, user: any) {}
